ca7e8ae479c39ad20796dee529774d09a15db883
Helena Paixão <hp.helenapaixao@gmail.com>
2020-01-16 19:45:17 -0300

    add controller searchController

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,8 +1,15 @@
 const axios = require("axios");
 const Dev = require('../models/Dev')
 
+
+
 module.exports = {
-  async store(request, response) {
+  async index(request, response) {
+      const devs =await Dev.find();
+
+      return response.json(devs)
+  },
+    async store(request, response) {
 
     const { github_username, techs, latitude, longitude } = request.body;
     
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const app = express();
 mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-p1bcw.mongodb.net/week10?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+
 });
 app.use(express.json());
 app.use(routes);
