771a392f4c8d0e63b00e1972f6f73f51ca9598c6
Helena Paixão <hp.helenapaixao@gmail.com>
2020-01-16 19:27:14 -0300

    finished router dev

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -5,27 +5,33 @@ module.exports = {
   async store(request, response) {
 
     const { github_username, techs, latitude, longitude } = request.body;
-  
+    
+    let  dev = await Dev.findOne({github_username});
+    if(!dev) {
+      
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
   
-   const {name = login,avatar_url,bio } = apiResponse.data;
-  
-   const techsArray = techs.split(',').map(tech => tech.trim());
-  
-   const location = {
-     type:'Point',
-     coordinates: [longitude,latitude]
-   }
+    const {name = login,avatar_url,bio } = apiResponse.data;
    
-    const dev = await Dev.create({
-     github_username,
-     name,
-     avatar_url,
-     bio,
-     techs: techsArray,
-      location
-   })
+    const techsArray = techs.split(',').map(tech => tech.trim());
+   
+    const location = {
+      type:'Point',
+      coordinates: [longitude,latitude]
+    }
+    
+      dev = await Dev.create({
+      github_username,
+      name,
+      avatar_url,
+      bio,
+      techs: techsArray,
+       location
+    })
+
+    }
   
+
     return response.json(dev);
   } 
 }
