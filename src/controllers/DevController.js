d5688d85bfaba8701e542ed956a9bf132f7a5112
Helena Paixão <hp.helenapaixao@gmail.com>
2020-01-16 19:57:55 -0300

    add util parseStringAsArray

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,7 +1,6 @@
 const axios = require("axios");
-const Dev = require('../models/Dev')
-
-
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
   async index(request, response) {
@@ -14,19 +13,19 @@ module.exports = {
     const { github_username, techs, latitude, longitude } = request.body;
     
     let  dev = await Dev.findOne({github_username});
+
     if(!dev) {
       
     const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
   
     const {name = login,avatar_url,bio } = apiResponse.data;
    
-    const techsArray = techs.split(',').map(tech => tech.trim());
+    const techsArray = parseStringAsArray(techs);
    
     const location = {
       type:'Point',
       coordinates: [longitude,latitude]
     }
-    
       dev = await Dev.create({
       github_username,
       name,
--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -1,7 +1,18 @@
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
 
 module.exports = {
   async index(request,response) {
 
+      const {latitude,longitude,techs} = request.query;
+      const techsArray = parseStringAsArray(techs);
+
+      const devs = await Dev.find({
+        techs: {
+          $in: techsArray,
+        }
+      });
+      console.log(techsArray);
+      return response.json({ devs });
   }
 }
