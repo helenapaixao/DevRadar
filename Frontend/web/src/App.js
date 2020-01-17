7cc5e6cfbcb92b721897c09188e643c2bfa64608
Helena Paixão <hp.helenapaixao@gmail.com>
2020-01-17 20:51:06 -0300

    style sidebar

--- Frontend/web/src/App.js
+++ Frontend/web/src/App.js
@@ -1,34 +1,40 @@
-import React from 'react';
-import './global.css';
-import './App.css';
-
+import React from "react";
+import "./global.css";
+import "./App.css";
+import './Sidebar.css';
 
 function App() {
- return (
-   <div id="app">
-     <aside>
-      <strong>Cadastrar</strong>
-      <form>
-        <div class="input-block">
-        <label htmlFor="github_username">Usuario do Github</label>
-        <input name="github_username" id="github_username" required/>
-        </div>
-        <div class="input-block">
-        <label htmlFor="techs">Tecnologias</label>
-        <input name="techs" id="techs" required/>
-        </div>
-        <div class="input-block">
-        <label htmlFor="username_gitub">Usuario do Github</label>
-        <input name="github_username" id="username_github" required/>
-        </div>   
-      </form>
-     </aside>
-     <main>
+  return (
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuario do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
 
-     </main>
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
 
-   </div>
- )
+            <div class="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main />
+    </div>
+  );
 }
 
 export default App;
