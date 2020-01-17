708a55fa364cb3d665254813d071a80d1580583e
Helena Paixão <hp.helenapaixao@gmail.com>
2020-01-17 20:06:34 -0300

    config page app

--- Frontend/web/src/App.js
+++ Frontend/web/src/App.js
@@ -9,8 +9,18 @@ function App() {
      <aside>
       <strong>Cadastrar</strong>
       <form>
-        <label htmlFor="">Usuario do Github</label>
-        <input name="github_username" id="username_github"/>
+        <div class="input-block">
+        <label htmlFor="github_username">Usuario do Github</label>
+        <input name="github_username" id="github_username" required/>
+        </div>
+        <div class="input-block">
+        <label htmlFor="techs">Tecnologias</label>
+        <input name="techs" id="techs" required/>
+        </div>
+        <div class="input-block">
+        <label htmlFor="username_gitub">Usuario do Github</label>
+        <input name="github_username" id="username_github" required/>
+        </div>   
       </form>
      </aside>
      <main>
