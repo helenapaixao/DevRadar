a0ef5850fb199d1d637cbbc04098ca48092efd4e
Helena Paixão <hp.helenapaixao@gmail.com>
2020-01-23 22:01:21 -0300

    config maps location

--- Mobile/mobile/src/pages/Main.js
+++ Mobile/mobile/src/pages/Main.js
@@ -1,8 +1,41 @@
-import React from 'react';
-import {View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet} from 'react-native'
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
+
 
 function Main() {
-  return <View/>
+
+  const [currentRegion,setCurrentRegion] = useState(null);
+  useEffect(() => {
+    async function loadInitialPosition() {
+      const {granted } = await requestPermissionsAsync();
+        if(granted) {
+          const {coords} = await getCurrentPositionAsync({
+            enableHighAccuracy: true,
+          });
+          const {latitude, longitude} = coords;
+
+          setCurrentRegion({
+            latitude,
+            longitude,
+            latitudeDelta: 0.04,
+            longitudeDelta:0.04,
+          })
+        }
+    }
+    loadInitialPosition();
+  },[]);
+  if(!currentRegion) {
+    return null;
+  }
+  return <MapView initRegion={currentRegion} style={sytles.map}/>
 }
+
+const sytles = StyleSheet.create({
+  map: {
+    flex:1
+  },
+})
 
 export default Main;
