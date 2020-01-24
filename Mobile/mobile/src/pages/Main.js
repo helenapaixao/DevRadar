26e3d10a083a965a465e942a05f2c5db65078273
Helena Paixão <hp.helenapaixao@gmail.com>
2020-01-23 22:25:45 -0300

    config maps in app

--- Mobile/mobile/src/pages/Main.js
+++ Mobile/mobile/src/pages/Main.js
@@ -1,11 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet,Image} from 'react-native'
-import MapView, {Marker} from 'react-native-maps';
-import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location'
+import { StyleSheet,Image } from 'react-native'
+import MapView, { Marker } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
 
 
 function Main() {
+
   const [currentRegion,setCurrentRegion] = useState(null);
+
   useEffect(() => {
     async function loadInitialPosition() {
       const {granted } = await requestPermissionsAsync();
@@ -25,22 +27,31 @@ function Main() {
     }
     loadInitialPosition();
   },[]);
+
   if(!currentRegion) {
     return null;
   }
   return (
-  <MapView initialRegion={currentRegion} style={sytles.map}>
+  <MapView initialRegion={currentRegion} style={styles.map}>
     <Marker coordinate={{latitude: -20.4868923, longitude: -54.5960898 }}>
-     <Image style={style.avatar} source={{uri: 'https://avatars1.githubusercontent.com/u/11083288?s=460&v=4'}}/>
+     <Image style={styles.avatar} source={{uri: 'https://avatars1.githubusercontent.com/u/11083288?s=460&v=4'}}/>
       </Marker>
     </MapView>
   );
 }
 
-const sytles = StyleSheet.create({
+const styles  = StyleSheet.create({
   map: {
+
     flex:1
   },
+  avatar: {
+    width: 54,
+    height: 54,
+    borderRadius:4,
+    borderWidth: 4,
+    borderColor: '#FFF'
+  }
 })
 
 export default Main;
